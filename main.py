c410242b61ada265df38bc6209cf5ef207cc8402
Qi Yang <96786782+qyscode@users.noreply.github.com>
2024-05-09 11:43:52 +0800

    Update main.py
    cool project! fixed a bug where '12345' is a possible word and enabled quitting and playing forever

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import csv
 all_words = []
 all_words_full = []
 all_letters = list(string.ascii_letters)
-
 available_letters = all_letters[:26]
+did_not_quit = True
 
 with open('5_letters.csv', 'r') as file:
     # Create a CSV reader object
@@ -16,100 +16,132 @@ with open('5_letters.csv', 'r') as file:
         # Append each row to the list
         all_words.append(row)
 
-for word in all_words:
+for word in all_words[1:]:
     current_word = ''
     for letter in word:
         current_word += letter
     all_words_full.append(current_word)
 
-mystery_word = random.choice(all_words_full)
-possible_characters = []
-correct_letters = []
+def game():
+    with open('5_letters.csv', 'r') as file:
+        # Create a CSV reader object
+        csv_reader = csv.reader(file)
 
-for letter in mystery_word:
-    possible_characters.append(letter)
+        for row in csv_reader:
+            # Append each row to the list
+            all_words.append(row)
 
-try_counter = 1
+    for word in all_words[1:]:
+        current_word = ''
+        for letter in word:
+            current_word += letter
+        all_words_full.append(current_word)
 
-solved = False
+    mystery_word = random.choice(all_words_full)
+    possible_characters = []
+    correct_letters = []
 
-def check_if_valid(word):
-    
-    if attempt.lower() not in all_words_full:
-        print('Not a valid word! Try again...')
-        return False
+    for letter in mystery_word:
+        possible_characters.append(letter)
 
-    else:
-        return True 
+    try_counter = 1
 
-while not solved and try_counter < 7:
+    solved = False
 
-    output = ''
-    while True:
-        attempt = input('Your Word: ')
-        if check_if_valid(attempt):
-            break
-
-    for i in range(5):
-        if attempt[i] == mystery_word[i]:
-            output += attempt[i].upper()
+    def check_if_valid(word):
+        
+        if attempt.lower() not in all_words_full:
+            print('Not a valid word! Try again...')
+            return False
 
         else:
-            output += attempt[i].lower()
-            if attempt[i].lower() in available_letters:
-                available_letters.remove(attempt[i].lower())
+            return True 
 
-        if attempt[i] in possible_characters:
-            if attempt[i] in correct_letters:
-                pass
-            
+    while not solved and try_counter < 7:
+
+        output = ''
+        while True:
+            attempt = input('\n\nType your word to try: \n\nType "q" to Quit\n')
+            if attempt in ['q','Q']:
+                return
+            elif check_if_valid(attempt):
+                break
+
+        for i in range(5):
+            if attempt[i] == mystery_word[i]:
+                output += attempt[i].upper()
+
             else:
-                correct_letters.append(attempt[i])
-        
-    print('Attempt N0.' + str(try_counter))
-    print('---------')
-    print(output)
-    print('---------')
-    print('Correct Letters: ')
-    print(correct_letters)
-    print('---------')
-    print('Available Letters: ') 
-    print(available_letters)
-    print('---------')
-    print('*********')
+                output += attempt[i].lower()
+                if attempt[i].lower() in available_letters:
+                    available_letters.remove(attempt[i].lower())
 
-    if attempt.lower() == mystery_word.lower():
-        solved = True 
-        print('You Win!!!!!')
+            if attempt[i] in possible_characters:
+                if attempt[i] in correct_letters:
+                    pass
+                
+                else:
+                    correct_letters.append(attempt[i])
+            
+        print('\n\nAttempt number ' + str(try_counter))
+        print('---------')
+        print(output)
+        print('---------')
+        print('Correct Letters: ')
+        print(correct_letters)
+        print('---------')
+        print('Available Letters: ') 
+        print(available_letters)
+        print('---------')
+        print('*********\n\n')
 
-    try_counter += 1 
+        if attempt.lower() == mystery_word.lower():
+            solved = True 
+            print('You Win!!!!!')
+            return
 
-if not solved:
-    print('You Lose :(')
-    print('Myster Word is ' + mystery_word)
-        
-    print("██████                        ██████    ")
-    print("██████████  ████████████████  ██████████")
-    print("█████████████              ██████████████")
-    print("████████                          ███████")
-    print("██████                              █████")
-    print("  ██                                  ██  ")
-    print("  ██                                  ██  ")
-    print("██        ██████            ██████        ██")
-    print("██      ██████████        ██████████      ██")
-    print("██    ████████  ██        ██  ████████    ██")
-    print("██    ████████  ██        ██  ████████    ██")
-    print("██    ██████████            ██████████    ██")
-    print("██      ██████      ████      ██████      ██")
-    print("  ██                ████                ██  ")
-    print("  ████████  ▒▒▒▒▒▒        ▒▒▒▒▒▒  ████████  ")
-    print("███████████▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒████████████")
-    print("██████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████")
-    print("██████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████")
-    print("██████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████")
-    print("  ████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████████  ")
-    print("    ████████  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ████████  ")
-    print("                ▒▒▒▒▒▒▒▒▒▒▒▒                ")
-    print("                  ▒▒▒▒▒▒▒▒                  ")
-    print("                    ▒▒▒▒                    ")
-        
+        try_counter += 1 
+
+    if not solved:
+        print('You Lose :(')
+        print('Myster Word is ' + mystery_word)
+            
+        print("██████                        ██████    ")
+        print("██████████  ████████████████  ██████████")
+        print("█████████████              ██████████████")
+        print("████████                          ███████")
+        print("██████                              █████")
+        print("  ██                                  ██  ")
+        print("  ██                                  ██  ")
+        print("██        ██████            ██████        ██")
+        print("██      ██████████        ██████████      ██")
+        print("██    ████████  ██        ██  ████████    ██")
+        print("██    ████████  ██        ██  ████████    ██")
+        print("██    ██████████            ██████████    ██")
+        print("██      ██████      ████      ██████      ██")
+        print("  ██                ████                ██  ")
+        print("  ████████  ▒▒▒▒▒▒        ▒▒▒▒▒▒  ████████  ")
+        print("███████████▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒████████████")
+        print("██████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████")
+        print("██████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████")
+        print("██████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████")
+        print("  ████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████████  ")
+        print("    ████████  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ████████  ")
+        print("                ▒▒▒▒▒▒▒▒▒▒▒▒                ")
+        print("                  ▒▒▒▒▒▒▒▒                  ")
+        print("                    ▒▒▒▒                    ")
+        return
+
+
+if input('Press "S" to play\n') in ["S","s"] :
+    game()
+
+while did_not_quit:
+    try_again = input('\n\nTry again?\ny/n\n')
+    if try_again in ["Y","y"]:
+        game()
+    elif try_again in ["N","n"]:
+        did_not_quit = False
+    else:
+        print("\nHuh?")
+        did_not_quit = False
